77be07cb1039e05f2c03fd469a6701f9b2d42149
webertrevisan <weber.trevisan@gmail.com>
2026-04-01 21:43:04 -0300

    study: adiciona exemplo de query params com URL no servidor

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,6 +2,17 @@ const users = require("../mocks/users");
 
 module.exports = {
   listUsers(request, response) {
+    const { order } = request.query;
+
+    const sortedUsers = users.sort((a, b)=> {
+      if (order === 'desc'){
+        return a.id <b.id ? 1 : -1;
+      }
+      
+      return a.id > b.id ? 1 : -1;
+    });
+
+    console.log(request.query)
     response.writeHead(200, { "content-type": "application/json" });
     response.end(JSON.stringify(users));
   },
--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 const http = require("http");
+const { URL } = require("url");
 
 const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = new URL(request.url, "http://localhost:3000");
+
+  console.log(Object.fromEntries(parsedUrl.searchParams));
+
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
   const route = routes.find(
     (routeObj) =>
-      routeObj.endpoint === request.url && routeObj.method === request.method,
+      routeObj.endpoint === parsedUrl.pathname &&
+      routeObj.method === request.method,
   );
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
     route.handler(request, response);
   } else {
     response.writeHead(404, { "content-type": "text/html" });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 
